db6ec4ff8b2cbf029873c0996499f02d75fff29d
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2020-08-21 23:24:31 +0100

    removed redundant pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,41 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GridOneComponent } from './components/grid-one/grid-one.component';
-import { GridTwoComponent } from './components/grid-two/grid-two.component';
-import { GridThreeComponent } from './components/grid-three/grid-three.component';
-import { GridFourComponent } from './components/grid-four/grid-four.component';
-import { RouterModule, Routes } from '@angular/router';
 import { IgxGridModule, IgxCheckboxModule } from 'igniteui-angular';
 import { SortingComponent } from './sorting/sorting.component';
 import { FilteringComponent } from './filtering/filtering.component';
 import { EditingComponent } from './editing/editing.component';
 import { PagingComponent } from './paging/paging.component';
 
-// export const routerConfig: Routes = [
-//   {
-//     path: 'grid-one',
-//     component: PagingComponent
-//   },
-//   {
-//     path: 'grid-two',
-//     component: SortingComponent
-//   }, {
-//     path: 'grid-one',
-//     component: FilteringComponent
-//   }, {
-//     path: 'grid-one',
-//     component: EditingComponent
-//   },
-// ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridOneComponent,
-    GridTwoComponent,
-    GridThreeComponent,
-    GridFourComponent,
     SortingComponent,
     FilteringComponent,
     EditingComponent,
@@ -48,7 +23,6 @@ import { PagingComponent } from './paging/paging.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // RouterModule.forRoot(routerConfig, { useHash: false }),
     IgxGridModule,
     IgxCheckboxModule
   ],
